b16a217c3c38282a24e9a83a316d398ee927eb66
CasaRol <casarol@PC1681.nordic.local>
2021-03-12 13:43:23 +0100

    Init commit: Created the baseline for the project layout with component and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { CataloguePageComponent } from './components/catalogue-page/catalogue-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { TrainerPageComponent } from './components/trainer-page/trainer-page.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    CataloguePageComponent,
+    HeaderComponent,
+    PokemonCardComponent,
+    TrainerPageComponent,
+    PokemonDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
