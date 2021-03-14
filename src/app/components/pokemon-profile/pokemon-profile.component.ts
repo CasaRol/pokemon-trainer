2ceeb972f58ead3e30f3fe82f66ddb620692a07a
Vytautas Zarauskas <v.zarauskas@gmail.com>
2021-03-14 22:57:08 +0100

    Feat: interim implementation of storing selected trainer pokemons

--- src/app/components/pokemon-profile/pokemon-profile.component.ts
+++ src/app/components/pokemon-profile/pokemon-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon.model';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-pokemon-profile',
@@ -9,7 +10,13 @@ import { Pokemon } from 'src/app/models/pokemon.model';
 export class PokemonProfileComponent implements OnInit {
   @Input() pokemon: Pokemon
 
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService) { }
+
+  onSelectClicked() {
+    console.log(this.pokemon.name);
+    console.log(this.pokemon.url);
+    this.localStorageService.storeSelectedPokemon(this.pokemon);
+  }
 
   ngOnInit(): void {
   }
