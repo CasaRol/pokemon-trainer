47e67ff69b2416e358a320c4765f104e9d94c459
Vytautas Zarauskas <v.zarauskas@gmail.com>
2021-03-16 10:44:12 +0100

    Feat: Added success message on Pokemon select

--- src/app/components/pokemon-profile/pokemon-profile.component.ts
+++ src/app/components/pokemon-profile/pokemon-profile.component.ts
@@ -8,12 +8,15 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
   styleUrls: ['./pokemon-profile.component.css']
 })
 export class PokemonProfileComponent implements OnInit {
+  showSuccess: boolean = false;
+
   @Input() pokemon: Pokemon
 
   constructor(private localStorageService: LocalStorageService) { }
 
   onSelectClicked() {
     this.localStorageService.storeSelectedPokemon(this.pokemon);
+    this.showSuccess = true;
   }
 
   ngOnInit(): void {
